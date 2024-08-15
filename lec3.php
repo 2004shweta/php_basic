ed7d0fb1a6aaaddab6e48a95241edb3af1de00b4
2004shweta <156895477+2004shweta@users.noreply.github.com>
2024-08-16 00:10:35 +0530

    lec3

---
+++ lec3.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+<h2>Type Casting </h2>
+<pre>
+<?php
+$a=5;
+$b=5.454;
+$c="hello";
+$d=true;
+$e=NULL;
+
+$a=(string) $a;
+$b=(string) $b;
+$c=(string) $c;
+$d=(string) $d;
+$e=(string) $e;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+
+
+?>
+</pre>
+<p>dnkdjc</p>
+</body>
+</html>
