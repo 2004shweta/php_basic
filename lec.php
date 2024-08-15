b880d626d7ba0ddbe214dde0290e430a19513f63
2004shweta <156895477+2004shweta@users.noreply.github.com>
2024-08-16 00:03:52 +0530

    basic php

---
+++ lec.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h2> hello we are going to start php</h>";
+echo "<br>";
+echo "hello student<br>";
+
+
+echo "<h2> php is fun! <h2>";
+echo "hello world!<br>";
+echo "i am about to learn php!<br>";
+echo "this","string","was","made","with multiple parameters";
+echo 'thuis is single coated string';
+echo "<br>";
+echo "<br>";
---
+++ lec2.php
@@ -0,0 +1,63 @@
+<!-- <?php
+$x=10;
+$y=4;
+echo ($x + $y);
+echo "<br>";
+echo ($x - $y);
+echo "<br>";
+echo ($x * $y);
+echo "<br>";
+echo ($x / $y);
+echo "<br>";
+echo ($x % $y);
+echo "<br>";
+echo ($x ** $y);
+echo "<br>";
+?> -->
+<!-- 
+<?php
+$x=25;
+$y=35;
+$z="25";
+
+var_dump($x == $z);
+var_dump($x === $z);
+var_dump($x != $z);
+var_dump($x !== $z);
+var_dump($x >= $z);
+var_dump($x <= $z);
+var_dump($x < $z);
+var_dump($x > $z);
+
+?> -->
+
+
+<html>
+<body>
+<h2>Type Casting </h2>
+<pre>
+<?php
+$a=5;
+$b=5.454;
+$c="hello";
+$d=true;
+$e=NULL;
+
+$a=(string) $a;
+$b=(string) $b;
+$c=(string) $c;
+$d=(string) $d;
+$e=(string) $e;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+
+
+?>
+</pre>
+<p>dnkdjc</p>
+</body>
+</html>
