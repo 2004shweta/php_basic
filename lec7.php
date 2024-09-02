f35eb8efabcc172cff4d3ce7adc6106fcc523987
2004shweta <156895477+2004shweta@users.noreply.github.com>
2024-09-02 11:40:57 +0530

    adding more question

--- lec7.php
+++ lec7.php
@@ -3,8 +3,17 @@
 // if (--$y == 2 || $y xor --$y) {
 //     echo $y;
 // }
+//output==0
+
+// $x=0;
+// echo $x--!=++$x;
+
+//output==no output
 
 
-$x=0;
-echo $x--!=++$x;
+$auth=1;
+$status=1;
+if($result=(($auth==1)&& ($status!=0))){
+    print "result is $result<br/>";
+}
 ?>
