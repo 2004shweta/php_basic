f7805eb6dd611e76ed6bbcd9f079818081d625b1
2004shweta <156895477+2004shweta@users.noreply.github.com>
2024-09-05 23:32:29 +0530

    lec9

---
+++ lec9.php
@@ -0,0 +1,13 @@
+<?php
+function TV($string){
+    echo "my favourite tv show is ".$string;
+    function b(){
+        echo "in am here to spoil this code";
+    }
+}
+function b2(){
+    echo "i am here to spoil this code";
+}
+b2();
+    TV("sherlock");
+    ?>
